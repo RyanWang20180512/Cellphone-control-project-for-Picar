92e01aad6f24ef9efd71ce5c5e0786ef23332b28
Wang Ryan <wzy115101000180@outlook.com>
2018-07-07 00:07:26 +0800

    Use SharedPreferences to save IP and Port info this time

--- app/src/main/java/com/example/wzy11/mobilecontroller/StartActivity.java
+++ app/src/main/java/com/example/wzy11/mobilecontroller/StartActivity.java
@@ -1,6 +1,7 @@
 package com.example.wzy11.mobilecontroller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,8 +18,12 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        //Get the last IP and port value, and show them
+        getInfoFromSharedPreference();
         textViewIP=(TextView)findViewById(R.id.carIP);
         textViewPort=(TextView)findViewById(R.id.carPort);
+        textViewIP.setText(host);
+        textViewPort.setText(String.valueOf(port));
         Button buttonEnter=(Button)findViewById(R.id.enter);
         buttonEnter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +34,24 @@ public class StartActivity extends AppCompatActivity {
                 startMainActivityIntent.putExtra("tcpHost",host);
                 startMainActivityIntent.putExtra("tcpPort",port);
                 startActivity(startMainActivityIntent);
+                //Save the ip and port values
+                setInfoFromSharedPreference();
                 finish();
             }
         });
+    }
+
+    private void getInfoFromSharedPreference(){
+        SharedPreferences preferences=getSharedPreferences("connectPara",MODE_PRIVATE);
+        host=preferences.getString("IP","192.168.1.1");
+        port=preferences.getInt("Port",50);
+
+    }
+
+    private void setInfoFromSharedPreference(){
+        SharedPreferences.Editor editor=getSharedPreferences("connectPara",MODE_PRIVATE).edit();
+        editor.putString("IP",host);
+        editor.putInt("Port",port);
+        editor.apply();
     }
 }
